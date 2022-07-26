4c88837ac0135bb6b901d52e7365aaf429ead60c
Janrick Mulato <janrick.mulato@siteminder.com>
2022-07-26 15:56:28 +0800

    Added element plus ui support vue 3

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
 import router from "./router"
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus)
+app.use(router).mount('#app')
+app.mount('#app')
 
